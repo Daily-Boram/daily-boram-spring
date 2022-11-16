c3764800c386b1847b8d51fd2033f4b7b95349fb
munjeongmin <3330kys@dsm.hs.kr>
2022-11-17 01:56:35 +0900

    📑 :: auth details 생성

--- src/main/java/com/example/dailyboramspring/DailyBoramSpringApplication.java
+++ src/main/java/com/example/dailyboramspring/DailyBoramSpringApplication.java
@@ -2,7 +2,10 @@ package com.example.dailyboramspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DailyBoramSpringApplication {
 
