1082fd826284e199d2758d6db510888974f074a8
KimSeungJin <klli1naver@gmail.com>
2022-11-17 23:20:53 +0900

    📑 :: Series Repository 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/series/domain/repository/SeriesRepository.java
@@ -0,0 +1,7 @@
+package com.example.dailyboramspring.domain.series.domain.repository;
+
+import com.example.dailyboramspring.domain.series.domain.Series;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SeriesRepository extends CrudRepository<Series, Long> {
+}
