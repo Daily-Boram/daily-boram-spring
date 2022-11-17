098610dad550edc1abf5fd953e32e63cad79fed8
munjeongmin <3330kys@dsm.hs.kr>
2022-11-17 17:30:34 +0900

    ⚡️ :: enable Jpa auditing

--- src/main/java/com/example/dailyboramspring/DailyBoramSpringApplication.java
+++ src/main/java/com/example/dailyboramspring/DailyBoramSpringApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
 public class DailyBoramSpringApplication {
