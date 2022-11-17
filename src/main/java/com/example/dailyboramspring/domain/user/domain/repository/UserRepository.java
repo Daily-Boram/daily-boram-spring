360cec8a7a2ff4eff2b10a09a5589c40dada91e9
KimSeungJin <klli1naver@gmail.com>
2022-11-17 19:22:09 +0900

    ♻️ :: accountId -> email

--- src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByAccountId(String accountId);
+    Optional<User> findByEmail(String email);
 }
