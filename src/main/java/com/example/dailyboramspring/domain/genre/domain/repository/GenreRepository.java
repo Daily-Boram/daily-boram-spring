6a8106b0e6ab5ecc936186dfb2a6da294a917b0b
태라 <terra2007@naver.com>
2022-11-18 13:40:55 +0900

    🐛 :: Genre Entity 수정

--- src/main/java/com/example/dailyboramspring/domain/genre/domain/repository/GenreRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/genre/domain/repository/GenreRepository.java
@@ -4,5 +4,5 @@ import com.example.dailyboramspring.domain.genre.domain.Genre;
 import com.example.dailyboramspring.domain.series.domain.Series;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GenreRepository extends CrudRepository<Genre, Series>{
+public interface GenreRepository extends CrudRepository<Genre, Long>{
 }
