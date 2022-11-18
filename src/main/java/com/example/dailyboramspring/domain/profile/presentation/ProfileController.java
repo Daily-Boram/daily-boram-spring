b0e055e5ea6df779bb030a288e73516ed5eebbca
태라 <terra2007@naver.com>
2022-11-18 18:33:12 +0900

    📑 :: Profile Controller 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/profile/presentation/ProfileController.java
@@ -0,0 +1,22 @@
+package com.example.dailyboramspring.domain.profile.presentation;
+
+import com.example.dailyboramspring.domain.profile.presentation.dto.response.UserProfileResponse;
+import com.example.dailyboramspring.domain.profile.service.GetUserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/profile")
+@RestController
+public class ProfileController {
+
+    private final GetUserProfileService getUserProfileService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{user_id}")
+
+    public UserProfileResponse getUserProfile(@PathVariable(name = "user_id") Long id) {
+        return getUserProfileService.excute(id);
+    }
+}
