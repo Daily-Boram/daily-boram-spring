e13b3679b001d7d7e3abd875ceed0403d5865de1
태라 <terra2007@naver.com>
2022-11-18 18:32:16 +0900

    ⚡️ :: Repository 기능 추가

--- src/main/java/com/example/dailyboramspring/domain/series/domain/repository/SeriesRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/series/domain/repository/SeriesRepository.java
@@ -1,7 +1,12 @@
 package com.example.dailyboramspring.domain.series.domain.repository;
 
 import com.example.dailyboramspring.domain.series.domain.Series;
+import com.example.dailyboramspring.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SeriesRepository extends CrudRepository<Series, Long> {
+
+    public List<Series> findAllByUser(User user);
 }
--- src/main/java/com/example/dailyboramspring/domain/serieslike/domain/repository/SeriesLikeRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/serieslike/domain/repository/SeriesLikeRepository.java
@@ -1,8 +1,11 @@
 package com.example.dailyboramspring.domain.serieslike.domain.repository;
 
+import com.example.dailyboramspring.domain.series.domain.Series;
 import com.example.dailyboramspring.domain.serieslike.domain.SeriesLike;
 import com.example.dailyboramspring.domain.serieslike.domain.SeriesLikeId;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SeriesLikeRepository extends CrudRepository<SeriesLike, SeriesLikeId> {
+
+    public Long countSeriesLikesBySeries(Series series);
 }
