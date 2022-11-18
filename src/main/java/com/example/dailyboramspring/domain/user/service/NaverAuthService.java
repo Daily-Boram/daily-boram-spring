dae91e909a8be2f073dcb785c6792082d92015ae
태라 <terra2007@naver.com>
2022-11-19 00:10:24 +0900

    ⚡️ :: Profile 생성 로직

--- src/main/java/com/example/dailyboramspring/domain/user/service/NaverAuthService.java
+++ src/main/java/com/example/dailyboramspring/domain/user/service/NaverAuthService.java
@@ -1,7 +1,10 @@
 package com.example.dailyboramspring.domain.user.service;
 
+import com.example.dailyboramspring.domain.profile.domain.Profile;
+import com.example.dailyboramspring.domain.profile.domain.repository.ProfileRepository;
 import com.example.dailyboramspring.domain.user.domain.User;
 import com.example.dailyboramspring.domain.user.domain.repository.UserRepository;
+import com.example.dailyboramspring.domain.user.facade.UserFacade;
 import com.example.dailyboramspring.domain.user.presentation.dto.response.TokenResponse;
 import com.example.dailyboramspring.global.properties.NaverProperties;
 import com.example.dailyboramspring.global.security.jwt.JwtTokenProvider;
@@ -9,6 +12,7 @@ import com.example.dailyboramspring.infrastructure.feign.client.NaverAuthClient;
 import com.example.dailyboramspring.infrastructure.feign.client.NaverGetClient;
 import com.example.dailyboramspring.infrastructure.feign.dto.NaverUserResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -20,6 +24,8 @@ public class NaverAuthService {
     private final NaverProperties naverProperties;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
+    private final UserFacade userFacade;
 
     public TokenResponse get(String code) {
 
@@ -32,13 +38,24 @@ public class NaverAuthService {
 
         NaverUserResponse.Response response = naverGetClient.get(token).getResponse();
 
-        if (userRepository.findByEmail(response.getEmail()).isEmpty()) {
+        if (!userRepository.existsByEmail(response.getEmail())) {
             userRepository.save(
                     User.builder()
                             .email(response.getEmail())
                             .gender(response.getGender())
                             .age(response.getAge())
                             .phoneNumber(response.getMobile())
+                            .build()
+            );
+
+            profileRepository.save(
+                    Profile.builder()
+                            .Id(userFacade.getUserByEmail(response.getEmail()).getId())
+                            .nickname(RandomStringUtils.randomAlphanumeric(8))
+
+                            // 임시 Image link
+                            .image("naver.com")
+                            .cookie(0)
                             .build()
             );
         }
