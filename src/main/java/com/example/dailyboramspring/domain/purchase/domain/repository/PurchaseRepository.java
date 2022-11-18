08b7c508f6d5a07c590f7b060a9bbb07ff31144a
태라 <terra2007@naver.com>
2022-11-18 20:06:58 +0900

    ⚡️ :: Purchase Repository 기능 추가

--- src/main/java/com/example/dailyboramspring/domain/purchase/domain/repository/PurchaseRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/purchase/domain/repository/PurchaseRepository.java
@@ -1,7 +1,11 @@
 package com.example.dailyboramspring.domain.purchase.domain.repository;
 
 import com.example.dailyboramspring.domain.purchase.domain.Purchase;
+import com.example.dailyboramspring.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PurchaseRepository extends CrudRepository<Purchase, Long> {
+    List<Purchase> findAllByUser(User user);
 }
