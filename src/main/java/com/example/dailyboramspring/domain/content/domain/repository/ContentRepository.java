006bb6a96196d0814f2e56d73e60f15f89f98847
태라 <terra2007@naver.com>
2022-11-18 14:17:02 +0900

    📑 :: Content Repository 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/content/domain/repository/ContentRepository.java
@@ -0,0 +1,7 @@
+package com.example.dailyboramspring.domain.content.domain.repository;
+
+import com.example.dailyboramspring.domain.content.domain.Content;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContentRepository extends CrudRepository<Content, Long> {
+}
