8b7816dcfa443c4181ea6cd01f6377bb62ba9bcd
태라 <terra2007@naver.com>
2022-11-18 13:24:20 +0900

    📑 :: Episode Entity 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/episode/domain/Episode.java
@@ -0,0 +1,37 @@
+package com.example.dailyboramspring.domain.episode.domain;
+
+import com.example.dailyboramspring.domain.series.domain.Series;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tbl_episode")
+@Entity
+public class Episode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "series_id", nullable = false)
+    private Series series;
+
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(25)")
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(400)")
+    private String summary;
+
+    @NotNull
+    @Column(columnDefinition = "VARCHAR(500)")
+    private String image;
+
+}
