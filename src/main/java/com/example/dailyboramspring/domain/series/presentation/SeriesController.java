1417907a5210e340a64942ac0a563c34cb5d7d71
KimSeungJin <klli1naver@gmail.com>
2022-11-18 19:05:33 +0900

    📑 :: seriesControoller 추가

---
+++ src/main/java/com/example/dailyboramspring/domain/series/presentation/SeriesController.java
@@ -0,0 +1,21 @@
+package com.example.dailyboramspring.domain.series.presentation;
+
+import com.example.dailyboramspring.domain.series.domain.repository.SeriesRepository;
+import com.example.dailyboramspring.domain.series.presentation.dto.requset.CreateSeriesRequest;
+import com.example.dailyboramspring.domain.series.service.CreateSeries;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/series")
+public class SeriesController {
+
+    private final CreateSeries createSeries;
+
+    public void postSeries(@RequestBody CreateSeriesRequest request){
+        createSeries.execute(request);
+    }
+}
