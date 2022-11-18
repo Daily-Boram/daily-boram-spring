d94a630c98b3144824dc14bcb5e170e53e04de00
태라 <terra2007@naver.com>
2022-11-19 00:10:04 +0900

    ⚡️ :: UserRepository existsByEmail 함수 추가

--- src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
