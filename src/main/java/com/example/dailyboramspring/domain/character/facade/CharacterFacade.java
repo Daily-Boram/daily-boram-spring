ba9ef6f60dc77ca5957ae624492b08404d41a33b
munjeongmin <3330kys@dsm.hs.kr>
2022-11-20 00:51:42 +0900

    📑 :: Character Facade 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/character/facade/CharacterFacade.java
@@ -0,0 +1,19 @@
+package com.example.dailyboramspring.domain.character.facade;
+
+import com.example.dailyboramspring.domain.character.domain.Character;
+import com.example.dailyboramspring.domain.character.domain.repository.CharacterRepository;
+import com.example.dailyboramspring.domain.character.exception.CharacterNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CharacterFacade {
+
+    private final CharacterRepository characterRepository;
+
+    public Character findCharacterById(Long characterId) {
+        return characterRepository.findById(characterId)
+                .orElseThrow(() -> CharacterNotFoundException.EXCEPTION);
+    }
+}
