f44007a7f349a847c299690c9a14e588931a2306
태라 <terra2007@naver.com>
2022-11-19 10:40:50 +0900

    ♻️ :: 코드 정렬

--- src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/user/domain/repository/UserRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
     Boolean existsByEmail(String email);
 }
