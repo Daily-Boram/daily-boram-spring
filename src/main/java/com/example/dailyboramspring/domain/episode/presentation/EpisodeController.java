d88fc8636fb26f68782c38b10c518448aa9cbec0
munjeongmin <3330kys@dsm.hs.kr>
2022-11-19 22:05:21 +0900

    ⚡️ :: 컨트롤러 등록

--- src/main/java/com/example/dailyboramspring/domain/episode/presentation/EpisodeController.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/presentation/EpisodeController.java
@@ -1,7 +1,9 @@
 package com.example.dailyboramspring.domain.episode.presentation;
 
 import com.example.dailyboramspring.domain.episode.presentation.dto.request.CreateEpisodeRequest;
+import com.example.dailyboramspring.domain.episode.presentation.dto.response.GetEpisodeResponse;
 import com.example.dailyboramspring.domain.episode.service.CreateEpisodeService;
+import com.example.dailyboramspring.domain.episode.service.GetEpisodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +14,16 @@ import org.springframework.web.bind.annotation.*;
 public class EpisodeController {
 
     private final CreateEpisodeService createEpisodeService;
+    private final GetEpisodeService getEpisodeService;
 
     @PostMapping("/{series-id}")
     @ResponseStatus(HttpStatus.CREATED)
     public void createEpisode(@PathVariable("series-id") Long seriesId, @RequestBody CreateEpisodeRequest request) {
         createEpisodeService.createEpisode(seriesId, request);
+    }
+
+    @GetMapping("/{episode-id}")
+    public GetEpisodeResponse getEpisode(@PathVariable("episode-id") Long episodeId) {
+        return getEpisodeService.getEpisode(episodeId);
     }
 }
