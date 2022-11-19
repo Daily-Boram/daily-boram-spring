b1c1ada2bc922f810107d05b13236da48f5a4d21
munjeongmin <3330kys@dsm.hs.kr>
2022-11-20 00:52:00 +0900

    📑 :: CreateContent 서비스 생성

---
+++ src/main/java/com/example/dailyboramspring/domain/content/presentation/dto/request/CreateContentRequest.java
@@ -0,0 +1,10 @@
+package com.example.dailyboramspring.domain.content.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateContentRequest {
+    private String line;
+}
