f12bb341afa8db7d89c6d139aff4eba1e8ed7ce9
munjeongmin <3330kys@dsm.hs.kr>
2022-11-20 23:25:09 +0900

    ⚡️ :: seriresList response 네임 재설정

--- src/main/java/com/example/dailyboramspring/domain/series/domain/repository/SeriesRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/series/domain/repository/SeriesRepository.java
@@ -3,11 +3,9 @@ package com.example.dailyboramspring.domain.series.domain.repository;
 import com.example.dailyboramspring.domain.genre.domain.Genre;
 import com.example.dailyboramspring.domain.series.domain.Series;
 import com.example.dailyboramspring.domain.user.domain.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-
 import java.util.Optional;
 
 public interface SeriesRepository extends CrudRepository<Series, Long> {
