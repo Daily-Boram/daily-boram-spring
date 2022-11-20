531ee692046cbea9bbfa858eb27f6c1a83a22922
KimSeungJin <klli1naver@gmail.com>
2022-11-20 14:04:48 +0900

    📑 :: findAllBySeries 생성

--- src/main/java/com/example/dailyboramspring/domain/genre/domain/repository/GenreRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/genre/domain/repository/GenreRepository.java
@@ -4,5 +4,8 @@ import com.example.dailyboramspring.domain.genre.domain.Genre;
 import com.example.dailyboramspring.domain.series.domain.Series;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GenreRepository extends CrudRepository<Genre, Long> {
+    List<Genre> findAllBySeries(Series series);
 }
