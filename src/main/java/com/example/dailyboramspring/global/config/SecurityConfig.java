8e26ae0ca488d5c8d5f7b54d3d683f714d4571fe
KimSeungJin <klli1naver@gmail.com>
2022-11-20 23:53:28 +0900

    asdads

--- src/main/java/com/example/dailyboramspring/global/config/SecurityConfig.java
+++ src/main/java/com/example/dailyboramspring/global/config/SecurityConfig.java
@@ -31,34 +31,34 @@ public class SecurityConfig {
         http
                 .authorizeRequests()
 
-                .antMatchers(HttpMethod.GET, "/login/oauth2/code/naver").permitAll()
+                .antMatchers(HttpMethod.GET, "/login/oauth2/code/naver").authenticated()
 
-                .antMatchers(HttpMethod.GET, "/main").permitAll()
+                .antMatchers(HttpMethod.GET, "/main").authenticated()
 
                 .antMatchers(HttpMethod.POST, "/series").authenticated()
                 .antMatchers(HttpMethod.GET, "/series/{series-id").authenticated()
 
-                .antMatchers(HttpMethod.GET, "/search").permitAll()
+                .antMatchers(HttpMethod.GET, "/search").authenticated()
 
-                .antMatchers(HttpMethod.POST, "/episode/{series-id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/episode/{series-id}").authenticated()
                 .antMatchers(HttpMethod.POST, "/episode/{episode-id}").authenticated()
 
-                .antMatchers(HttpMethod.POST, "/content/{episode-id}/{character-id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/content/{episode-id}/{character-id}").authenticated()
 
-                .antMatchers(HttpMethod.GET, "/profile/other/{user_id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/profile/me").permitAll()
-                .antMatchers(HttpMethod.PATCH, "/profile").permitAll()
+                .antMatchers(HttpMethod.GET, "/profile/other/{user_id}").authenticated()
+                .antMatchers(HttpMethod.GET, "/profile/me").authenticated()
+                .antMatchers(HttpMethod.PATCH, "/profile").authenticated()
 
-                .antMatchers(HttpMethod.POST, "/like/episode/{episode_id}").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/like/episode/{episode_id}").permitAll()
-                .antMatchers(HttpMethod.POST, "/like/series/{series-id}").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/like/series/{episode_id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/like/episode/{episode_id}").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/like/episode/{episode_id}").authenticated()
+                .antMatchers(HttpMethod.POST, "/like/series/{series-id}").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/like/series/{episode_id}").authenticated()
 
-                .antMatchers(HttpMethod.POST, "/character/{series-id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/character/{series-id}").authenticated()
 
-                .antMatchers(HttpMethod.POST, "/comment/{episode-id}").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/comment/{episode-id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/comment/{episode-id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/comment/{episode-id}").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/comment/{episode-id}").authenticated()
+                .antMatchers(HttpMethod.GET, "/comment/{episode-id}").authenticated()
 
                 .anyRequest().authenticated()
                 .and()
