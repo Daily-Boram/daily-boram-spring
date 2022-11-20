1162d23097cb77923f903c4056d5be960d3eb1a8
KimSeungJin <klli1naver@gmail.com>
2022-11-20 14:03:24 +0900

    📑 :: findAllBySeries 생성

--- src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
@@ -1,7 +1,12 @@
 package com.example.dailyboramspring.domain.episode.domain.repository;
 
 import com.example.dailyboramspring.domain.episode.domain.Episode;
+import com.example.dailyboramspring.domain.series.domain.Series;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EpisodeRepository extends CrudRepository<Episode, Long> {
+
+    List<Episode> findAllBySeries(Series series);
 }
