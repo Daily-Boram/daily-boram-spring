584e679838f76683f8ba5d9274ee1267e36bb720
태라 <terra2007@naver.com>
2022-11-20 13:20:59 +0900

    ⚡️ :: EpisodeRepository 함수 생성

--- src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
@@ -4,4 +4,5 @@ import com.example.dailyboramspring.domain.episode.domain.Episode;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EpisodeRepository extends CrudRepository<Episode, Long> {
+    Boolean existsEpisodeById(Long id);
 }
