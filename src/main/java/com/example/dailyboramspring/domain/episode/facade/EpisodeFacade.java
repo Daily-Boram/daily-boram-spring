441458bbcbd110a7ce07004de7108b8ab9bdea8f
KimSeungJin <klli1naver@gmail.com>
2022-11-20 14:04:01 +0900

    📑 :: getEpisodeBySeries 생성

--- src/main/java/com/example/dailyboramspring/domain/episode/facade/EpisodeFacade.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/facade/EpisodeFacade.java
@@ -3,8 +3,11 @@ package com.example.dailyboramspring.domain.episode.facade;
 import com.example.dailyboramspring.domain.episode.domain.Episode;
 import com.example.dailyboramspring.domain.episode.domain.repository.EpisodeRepository;
 import com.example.dailyboramspring.domain.episode.exception.EpisodeNotFoundException;
+import com.example.dailyboramspring.domain.series.domain.Series;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -15,5 +18,10 @@ public class EpisodeFacade {
     public Episode getEpisodeById(Long id) {
         return episodeRepository.findById(id)
                 .orElseThrow(() -> EpisodeNotFoundException.EXCEPTION);
+    }
+
+    public List<Episode> getEpisodeBySeries(Series series){
+        return episodeRepository.findAllBySeries(series);
+
     }
 }
