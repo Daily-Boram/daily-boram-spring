8e093dc053983644351e58f0c170fe3d21146dc4
KimSeungJin <klli1naver@gmail.com>
2022-11-28 18:50:47 +0900

    🐛 :: import 오류 수정

--- src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
@@ -2,9 +2,10 @@ package com.example.dailyboramspring.domain.episode.domain.repository;
 
 import com.example.dailyboramspring.domain.episode.domain.Episode;
 import com.example.dailyboramspring.domain.series.domain.Series;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 
 public interface EpisodeRepository extends CrudRepository<Episode, Long> {
