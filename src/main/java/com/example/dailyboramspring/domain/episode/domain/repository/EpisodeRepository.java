46e67d01d2037ff1ec7e4113372f5ac5cd28f8cc
KimSeungJin <klli1naver@gmail.com>
2022-11-28 15:42:54 +0900

    📑 :: findAllBySeriesOrdetByIdAsc 추가

--- src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/domain/repository/EpisodeRepository.java
@@ -4,12 +4,15 @@ import com.example.dailyboramspring.domain.episode.domain.Episode;
 import com.example.dailyboramspring.domain.series.domain.Series;
 import org.springframework.data.repository.CrudRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface EpisodeRepository extends CrudRepository<Episode, Long> {
 
 
     List<Episode> findAllBySeries(Series series);
+
+    List<Episode> findAllBySeriesOrderByIdAsc(Series series, Pageable pageable);
 
     Boolean existsEpisodeById(Long id);
 
