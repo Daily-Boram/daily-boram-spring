730b730da4b9c749d74445f09b5d42674a58f261
KimSeungJin <klli1naver@gmail.com>
2022-11-28 15:43:26 +0900

    📑 :: 매소드 변경

--- src/main/java/com/example/dailyboramspring/domain/series/service/GetSeriesDetailService.java
+++ src/main/java/com/example/dailyboramspring/domain/series/service/GetSeriesDetailService.java
@@ -1,5 +1,6 @@
 package com.example.dailyboramspring.domain.series.service;
 
+import com.example.dailyboramspring.domain.episode.domain.repository.EpisodeRepository;
 import com.example.dailyboramspring.domain.episode.facade.EpisodeFacade;
 import com.example.dailyboramspring.domain.genre.domain.repository.GenreRepository;
 import com.example.dailyboramspring.domain.genre.facade.GenreFacade;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,16 +25,16 @@ public class GetSeriesDetailService {
 
     private final SeriesFacade seriesFacade;
     private final SeriesLikeFacade seriesLikeFacade;
-    private final EpisodeFacade episodeFacade;
+    private final EpisodeRepository episodeRepository;
     private final GenreFacade genreFacade;
     private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
-    public SeriesDetailResponse execute(Long seriesId) {
+    public SeriesDetailResponse execute(Long seriesId, Pageable pageable) {
         User user = userFacade.getCurrentUser();
         Series series = seriesFacade.findById(seriesId);
 
-        List<EpisodeListElement> episodeLists = episodeFacade.getEpisodeBySeries(series)
+        List<EpisodeListElement> episodeLists = episodeRepository.findAllBySeriesOrderByIdAsc(series, pageable)
                 .stream()
                 .map(episode -> EpisodeListElement.builder()
                         .id(episode.getId())
