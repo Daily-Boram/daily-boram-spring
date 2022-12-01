0d64867a15bb9ed1a0fc8b4c8ae67fd469f2fe98
태라 <terra2007@naver.com>
2022-12-01 23:33:44 +0900

    ⚡️ :: NotNull, NotBlank 수정

--- src/main/java/com/example/dailyboramspring/domain/episode/presentation/dto/request/CreateEpisodeRequest.java
+++ src/main/java/com/example/dailyboramspring/domain/episode/presentation/dto/request/CreateEpisodeRequest.java
@@ -4,16 +4,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class CreateEpisodeRequest {
+    @NotBlank
     private String title;
+    @NotNull
     private Integer cost;
+    @NotBlank
     private String image;
+    @NotBlank
     private String imageContent;
+    @NotNull
     private List<Character_list> character;
+    @NotNull
     private List<Content_list> content;
 
     @Getter
